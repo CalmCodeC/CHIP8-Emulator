87042127b16d6b52af8acfdee07c1d798823c4ee
Jesalvein <ccchesschess@gmail.com>
2015-08-05 23:12:36 +1400

    patch up !

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <GL/glut.h>
 #include "chip8.h"
+#include <iostream.h>
 
 // Display size
 #define SCREEN_WIDTH 64
@@ -28,7 +29,7 @@ int main(int argc, char **argv)
 {
 	if(argc < 2)
 	{
-		printf("Usage: myChip8.exe chip8application\n\n");
+		printf("Initialized the Program. \n\n");
 		return 1;
 	}
 
@@ -36,53 +37,48 @@ int main(int argc, char **argv)
 	if(!myChip8.loadApplication(argv[1]))
 		return 1;
 
-	// Setup OpenGL
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-
-	glutInitWindowSize(display_width, display_height);
-    glutInitWindowPosition(320, 320);
+	// glut start...
+	glutInit(&argc, argv); glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+        glutInitWindowSize(display_width, display_height);glutInitWindowPosition(640, 480);
 	glutCreateWindow("Super Storm CHIP-8 Emulator");
 
 	glutDisplayFunc(display);
 	glutIdleFunc(display);
     glutReshapeFunc(reshape_window);
-	glutKeyboardFunc(keyboardDown);
-	glutKeyboardUpFunc(keyboardUp);
+	glutKeyboardFunc(keyboardDown); // initialises key.
+	glutKeyboardUpFunc(keyboardUp); // Removes the initialised key.
 
 #ifdef DRAWWITHTEXTURE
 	setupTexture();
 #endif
 
-	glutMainLoop();
+	glutMainLoop(); // the render loop for textures.
 
 	return 0;
 }
 
-// Setup Texture
 void setupTexture()
 {
-	// Clear screen
 	for(int y = 0; y < SCREEN_HEIGHT; ++y)
 		for(int x = 0; x < SCREEN_WIDTH; ++x)
-			screenData[y][x][0] = screenData[y][x][1] = screenData[y][x][2] = 0;
+			screenData[y][x][0] = screenData[y][x][1] = screenData[y][x][2] = 0; // clear it up 
 
-	// Create a texture
+	/
 	glTexImage2D(GL_TEXTURE_2D, 0, 3, SCREEN_WIDTH, SCREEN_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)screenData);
 
-	// Set up the texture
+	
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
 
-	// Enable textures
+	// needed for textures 
 	glEnable(GL_TEXTURE_2D);
 }
 
 void updateTexture(const chip8& c8)
 {
-	// Update pixels
+	
 	for(int y = 0; y < 32; ++y)
 		for(int x = 0; x < 64; ++x)
 			if(c8.gfx[(y * 64) + x] == 0)
@@ -133,7 +129,7 @@ void display()
 
 	if(myChip8.drawFlag)
 	{
-		// Clear framebuffer
+		
 		glClear(GL_COLOR_BUFFER_BIT);
 
 #ifdef DRAWWITHTEXTURE
@@ -142,10 +138,8 @@ void display()
 		updateQuads(myChip8);
 #endif
 
-		// Swap buffers!
 		glutSwapBuffers();
 
-		// Processed frame
 		myChip8.drawFlag = false;
 	}
 }
@@ -189,7 +183,7 @@ void keyboardDown(unsigned char key, int x, int y)
 	else if(key == 'c')	myChip8.key[0xB] = 1;
 	else if(key == 'v')	myChip8.key[0xF] = 1;
 
-	//printf("Press key %c\n", key);
+	cout<<"Pressed key"<<key; // for debugging purposes 
 }
 
 void keyboardUp(unsigned char key, int x, int y)
